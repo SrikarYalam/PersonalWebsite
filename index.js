314e93daa323d24db499c1fb5e3ba7f9ed79c6f0
Srikar Yalam <srikar.yalamsy@gmail.com>
2024-07-15 23:19:01 -0500

    will have to adjust a bit of the html to get functionality to work with the wiggle

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ const experienceView = () => {
 const projectsView = () => {
 
 }
+
+
+/* Event Listeners */
+const allPossibleHighlights = ["skills", "experiences", "projects"];
+
+for (const possibleHighlight of allPossibleHighlights) {
+    const hiddenLetters = document.getElementsByClassName(`hidden-letter ${possibleHighlight}-highlight`);
+    const highlights = document.getElementsByClassName(`${possibleHighlight}-highlight`);
+
+    for (const highlight of highlights) {
+        highlight.addEventListener("mouseover", () => {
+            for (const hiddenSkillsLetter of hiddenLetters) {
+                hiddenSkillsLetter.style.display = "inline-block";
+                for (const hl of highlights)
+                {
+                    if (hl.className == highlight.className)
+                    {
+                        hl.style.animation = 'wiggle .1s infinite';
+                    }
+                }
+            }
+        })
+        highlight.addEventListener("mouseout", () => {
+            for (const hiddenSkillsLetter of hiddenLetters) {
+                hiddenSkillsLetter.style.display = "none";
+            }
+        })
+    }
+}
